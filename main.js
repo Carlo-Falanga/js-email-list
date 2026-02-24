b584d32d0fc365cc60017fed2ef1f3210674863d
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-24 11:40:50 +0100

    complete bonus

--- main.js
+++ main.js
@@ -1,22 +1,27 @@
-const ulEl = document.getElementById('list')
+const ulEl = document.getElementById("list");
+const btnRefreshEl = document.getElementById("js_btn_refresh");
 
-
-let mailsArr = []
-
-for (let i = 0; i < 10; i++){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then(resp => {
+function apiMail(){
+  ulEl.innerHTML = '';
+  let mailsArr = [];
+  for (let i = 0; i < 10; i++) {
+    axios
+      .get("https://flynn.boolean.careers/exercises/api/random/mail")
+      .then((resp) => {
         const apiMails = resp.data.response;
-        mailsArr.push(apiMails)
-
-        if (mailsArr.length === 10){
-            
-            for (let i = 0; i < mailsArr.length; i++){
-                const mail = mailsArr[i]
-                const markup = `<li>${mail}</li>`
-                ulEl.innerHTML += markup
-                
-            }
+        mailsArr.push(apiMails);
+  
+        if (mailsArr.length === 10) {
+          for (let i = 0; i < mailsArr.length; i++) {
+            const mail = mailsArr[i];
+            const markup = `<li>${mail}</li>`;
+            ulEl.innerHTML += markup;
+          }
         }
-    })
+      });
+    }
+
 }
+
+
+btnRefreshEl.addEventListener('click', apiMail);
